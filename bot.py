35225ba029f8c5b6fc97b0d2bdbe8c7749f7461e
nikname424 <131608242+nikname424@users.noreply.github.com>
2024-09-10 17:15:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,12 +10,7 @@ bot = AsyncTeleBot(TOKEN)
 async def start(message: telebot.types.Message): #функция для обработки
     await bot.send_message(chat_id=message.from_user.id, text='hello', reply_markup=mainButtonInline) #отправка сообщения
 
-@bot.message_handler(commands=['photo']) #для отправки фотографии
-async def photo(message: telebot.types.Message): #функция для обработки
-    with open('main_icon.png', 'rb') as file:
-        await bot.send_photo(chat_id=message.from_user.id, photo=file)
-
 async def main():
     await asyncio.gather(bot.polling())
 
-asyncio.run(main())
+asyncio.run(main()) 
